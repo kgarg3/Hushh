b447e1eb04c5a3f928fd4a94de35e9979232aa01
kgarg3 <kanan0003@gmail.com>
2014-03-02 21:42:16 -0800

    least one friend is selected.

--- Hush/src/com/hush/activities/PickFriendsActivity.java
+++ Hush/src/com/hush/activities/PickFriendsActivity.java
@@ -25,74 +25,72 @@ public class PickFriendsActivity extends FragmentActivity {
 		return true;
 	}
 
-    FriendPickerFragment friendPickerFragment;
+	FriendPickerFragment friendPickerFragment;
 
-    // A helper to simplify life for callers who want to populate a Bundle with the necessary
-    // parameters. A more sophisticated Activity might define its own set of parameters; our needs
-    // are simple, so we just populate what we want to pass to the FriendPickerFragment.
-    public static void populateParameters(Intent intent, String userId, boolean multiSelect, boolean showTitleBar) {
-        intent.putExtra(FriendPickerFragment.USER_ID_BUNDLE_KEY, userId);
-        intent.putExtra(FriendPickerFragment.MULTI_SELECT_BUNDLE_KEY, multiSelect);
-        intent.putExtra(FriendPickerFragment.SHOW_TITLE_BAR_BUNDLE_KEY, showTitleBar);
-    }
+	// A helper to simplify life for callers who want to populate a Bundle with the necessary
+	// parameters. A more sophisticated Activity might define its own set of parameters; our needs
+	// are simple, so we just populate what we want to pass to the FriendPickerFragment.
+	public static void populateParameters(Intent intent, String userId, boolean multiSelect, boolean showTitleBar) {
+		intent.putExtra(FriendPickerFragment.USER_ID_BUNDLE_KEY, userId);
+		intent.putExtra(FriendPickerFragment.MULTI_SELECT_BUNDLE_KEY, multiSelect);
+		intent.putExtra(FriendPickerFragment.SHOW_TITLE_BAR_BUNDLE_KEY, showTitleBar);
+	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pick_friends);
-        
-        FragmentManager fm = getSupportFragmentManager();
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pick_friends);
 
-        if (savedInstanceState == null) {
-            // First time through, we create our fragment programmatically.
-            final Bundle args = getIntent().getExtras();
-            friendPickerFragment = new FriendPickerFragment(args);
-            fm.beginTransaction().add(R.id.friend_picker_fragment, friendPickerFragment).commit();
-        } else {
-            // Subsequent times, our fragment is recreated by the framework and already has saved and
-            // restored its state, so we don't need to specify args again. (In fact, this might be
-            // incorrect if the fragment was modified programmatically since it was created.)
-            friendPickerFragment = (FriendPickerFragment) fm.findFragmentById(R.id.friend_picker_fragment);
-        }
+		FragmentManager fm = getSupportFragmentManager();
 
-        friendPickerFragment.setOnErrorListener(new PickerFragment.OnErrorListener() {
-            @Override
-            public void onError(PickerFragment<?> fragment, FacebookException error) {
-                PickFriendsActivity.this.onError(error);
-            }
-        });
+		if (savedInstanceState == null) {
+			// First time through, we create our fragment programmatically.
+			final Bundle args = getIntent().getExtras();
+			friendPickerFragment = new FriendPickerFragment(args);
+			fm.beginTransaction().add(R.id.friend_picker_fragment, friendPickerFragment).commit();
+		} else {
+			// Subsequent times, our fragment is recreated by the framework and already has saved and
+			// restored its state, so we don't need to specify args again. (In fact, this might be
+			// incorrect if the fragment was modified programmatically since it was created.)
+			friendPickerFragment = (FriendPickerFragment) fm.findFragmentById(R.id.friend_picker_fragment);
+		}
 
-        friendPickerFragment.setOnDoneButtonClickedListener(new PickerFragment.OnDoneButtonClickedListener() {
-            @Override
-            public void onDoneButtonClicked(PickerFragment<?> fragment) {
-                // We just store our selection in the Application for other activities to look at.
-                HushApp application = (HushApp) getApplication();
-                application.setSelectedUsers(friendPickerFragment.getSelection());
+		friendPickerFragment.setOnErrorListener(new PickerFragment.OnErrorListener() {
+			@Override
+			public void onError(PickerFragment<?> fragment, FacebookException error) {
+				PickFriendsActivity.this.onError(error);
+			}
+		});
 
-                setResult(RESULT_OK, null);
-                finish();
-            }
-        });
-    }
+		friendPickerFragment.setOnDoneButtonClickedListener(new PickerFragment.OnDoneButtonClickedListener() {
+			@Override
+			public void onDoneButtonClicked(PickerFragment<?> fragment) {
+				// We just store our selection in the Application for other activities to look at.
+				HushApp.setSelectedUsers(friendPickerFragment.getSelection());
 
-    private void onError(Exception error) {
-        String text = getString(R.string.facebook_exception, error.getMessage());
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
-    }
+				setResult(RESULT_OK, null);
+				finish();
+			}
+		});
+	}
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        try {
-            HushApp application = (HushApp) getApplication();
-            List<GraphUser> selectedUsers = application.getSelectedUsers();
-            if (selectedUsers != null && !selectedUsers.isEmpty()) {
-                friendPickerFragment.setSelection(selectedUsers);
-            }
-            // Load data, unless a query has already taken place.
-            friendPickerFragment.loadData(false);
-        } catch (Exception ex) {
-            onError(ex);
-        }
-    }
+	private void onError(Exception error) {
+		String text = getString(R.string.facebook_exception, error.getMessage());
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		try {
+			List<GraphUser> selectedUsers = HushApp.getSelectedUsers();
+			if (selectedUsers != null && !selectedUsers.isEmpty()) {
+				friendPickerFragment.setSelection(selectedUsers);
+			}
+			// Load data, unless a query has already taken place.
+			friendPickerFragment.loadData(false);
+		} catch (Exception ex) {
+			onError(ex);
+		}
+	}
 }
